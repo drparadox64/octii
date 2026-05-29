23a47b1ed2e03c9bf19757bc62da39ebb257aa6d
nuclearweb99 <steliospanag2012@gmail.com>
2026-05-29 23:06:53 +0300

    initial 02

---
+++ tiled/nh.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="nh" tilewidth="32" tileheight="32" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../gr/floor01.svg" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../gr/wall01.svg" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../gr/player.svg" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../gr/glider.svg" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../gr/fireball.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../gr/grbutton.svg" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../gr/grwall0.svg" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../gr/grwall1.svg" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../gr/slime.svg" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../gr/ball.svg" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../gr/chip.svg" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../gr/box.svg" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../gr/water.svg" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="../gr/dirt.svg" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="../gr/keys/rkey.svg" width="32" height="32"/>
+ </tile>
+  <tile id="15">
+  <image source="../gr/keys/gkey.svg" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="../gr/keys/bkey.svg" width="32" height="32"/>
+ </tile>
+  <tile id="17">
+  <image source="../gr/keys/ykey.svg" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="../gr/gates/rgate.svg" width="32" height="32"/>
+ </tile>
+  <tile id="19">
+  <image source="../gr/gates/ggate.svg" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="../gr/gates/bgate.svg" width="32" height="32"/>
+ </tile>
+  <tile id="21">
+  <image source="../gr/gates/ygate.svg" width="32" height="32"/>
+ </tile>
+  <tile id="22">
+  <image source="../gr/exit.svg" width="32" height="32"/>
+ </tile>
+  <tile id="23">
+  <image source="../gr/socket.svg" width="32" height="32"/>
+ </tile>
+  <tile id="24">
+  <image source="../gr/help.svg" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+ <image source="../gr/popupw.svg" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+ <image source="../gr/bomb.svg" width="32" height="32"/>
+ </tile>
+  <tile id="27">
+  <image source="../gr/powerups/firesh.svg" width="32" height="32"/>
+ </tile>
+  <tile id="28">
+  <image source="../gr/powerups/watersh.svg" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+ <image source="../gr/powerups/forcesh.svg" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+ <image source="../gr/powerups/icesh.svg" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+ <image source="../gr/gravel.svg" width="32" height="32"/>
+ </tile>
+</tileset>
